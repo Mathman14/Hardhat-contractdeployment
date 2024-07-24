93720dc69f230885dbf3208631ba7e2acf0d8c0f
Mathman14 <Muhammedahmedyahaya079@gmail.com>
2024-07-24 21:39:22 +0100

    hardhat contract deployment

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: ["_private_key.env"], //Your private key starting with "0x" 
+      //Make sure you have enough funds in this wallet to deploy the smart contract
+    },
+  },
+ 
+};
